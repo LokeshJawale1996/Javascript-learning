01e58388f300e8a4d8fac417f1d75a39384da8d0
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-03 23:23:45 +0530

    learning started from code with harry tutorial

---
+++ Code With Harry/js/index.js
@@ -0,0 +1,13 @@
+console.time("Your code Took");
+console.log("hello console");
+console.log(34 + 5);
+console.log(34);
+console.log(true);
+console.log([23, 22, 1, 34]);
+console.log({ name: "lokesh", age: "25" });
+console.table({ name: "lokesh", age: "25" });
+console.warn("this is warning");
+// console.clear();
+console.timeEnd("Your code Took");
+// console.assert(566 > 189, "age  > 189 is not possible");
+console.error("this is an error");
