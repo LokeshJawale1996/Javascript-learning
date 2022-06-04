1bf560aaf8b2826666a3c8b1f078be8903751642
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-04 17:59:33 +0530

    Dom understanding tutorial done

---
+++ Code With Harry/js/tut12.js
@@ -0,0 +1,15 @@
+console.log("Welcome to tutorial 12");
+
+let a = document;
+a = document.all;
+// a = document.body; //gives body of the website.html
+// a = document.forms; //gives forms of the website.html
+
+// Array.from(a).forEach(function (element) {
+//   console.log(element);
+// });
+
+a = document.links; //to find the links from document
+// a = document.images;//to find the images from document
+// a= document.scripts;//to finc the scripts from the document
+console.log(a);
