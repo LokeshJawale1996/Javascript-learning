300f7ea668ad52efee0ec9f6289a2a681b4aba38
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-04 17:20:44 +0530

    tutorial on window object done

---
+++ Code With Harry/js/tut11.js
@@ -0,0 +1,18 @@
+console.log("This is Tutorial 11");
+//Window Object
+//window is global object
+//i.e window.alert(') and alert(') gives us same output
+//we can execute all operations within window without using it window.
+
+let a = window;
+// window.alert("Hello Lokesh");//we get alert with Hello Lokesh
+// a = prompt("This will destroy your computer. Type your name"); //whatever we type in input box will be stored in variable a
+// a = confirm("Are you sure you want to delete this page?"); //it confirms if we ok the variables return true otherwise false
+// a = window.innerHeight;
+// a = innerWidth;
+// a = innerHeight;
+// a =scrollX;
+// a =scrollY;
+// a = location;
+// a = location.reload(); //to refresh the page
+console.log(a);
