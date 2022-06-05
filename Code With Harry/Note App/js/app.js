c37229d2b983ded56ac0c12fd6946d0fdce47f84
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-05 16:15:55 +0530

    note app(project 1 in process

---
+++ Code With Harry/Note App/js/app.js
@@ -0,0 +1,63 @@
+console.log("Welcome to notes app!");
+showNotes();
+
+//If user adds a note,add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addText = document.getElementById("addText");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addText.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+
+  addText.value = "";
+  console.log(notesObj);
+  showNotes();
+});
+
+//function to show elements from local Strorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+    <div class="noteCard my-2 mx-2 card" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text"> ${element} </p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+          </div>
+        </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = ` Nothing to show! Use "Add a Note" section above to add notes`;
+  }
+}
+
+//function to delete a notes
+function deleteNote(index) {
+  console.log("i am deleting", index);
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+
+  showNotes();
+}
