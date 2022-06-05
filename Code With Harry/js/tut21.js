526c6093dac9c09471bda0a64be8a8d3acddd22e
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-05 14:01:21 +0530

    file updated

---
+++ Code With Harry/js/tut21.js
@@ -0,0 +1,10 @@
+console.log("This is tutorial 21 Exercise 2 in Js");
+
+/*
+You have to create a dive and inject it inti the page which contains a heading
+
+whenever someone clicks on the div,it should be converted into an editable item.
+whenever user clicks away (blur) .
+save the note into localstorage.
+
+*/
