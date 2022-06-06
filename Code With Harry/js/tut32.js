b5b4fd90ad039447af033ab7fc263dfefe674336
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-06 22:43:26 +0530

    es6 classes and inheritance tut done

---
+++ Code With Harry/js/tut32.js
@@ -0,0 +1,6 @@
+console.log("This is tutorial 32");
+// create a class library and implement the following:
+// constructor must take the book list as an argument
+// getBookList()
+// issueBook(bookname, user)
+// returnBook(bookname)
