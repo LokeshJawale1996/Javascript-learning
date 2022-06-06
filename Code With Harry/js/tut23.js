2065f89d06eccc571d7ae1442f7c18c94d32a936
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-06 13:08:26 +0530

    tutorial on date and math obejct are done

---
+++ Code With Harry/js/tut23.js
@@ -0,0 +1,30 @@
+console.log("We are in tutorial 23");
+
+let x = 3;
+let y = 6;
+let z;
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
+
+// Exploring the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4); //5
+z = Math.ceil(5.3); //6
+z = Math.floor(-5.3); //-6
+z = Math.abs(-5); //5
+z = Math.sqrt(64); //8
+z = Math.pow(2, 3); //8
+z = Math.min(2, 3, 34, 234, 2342, 34); //2
+z = Math.max(2, 3, 34, 234, 34); //234
+z = Math.random();
+z = 100 * Math.random();
+z = Math.ceil(50 + (100 - 50) * Math.random());
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+
+console.log(z);
