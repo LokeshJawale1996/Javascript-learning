1ef1877d507ba83e0e4c43934ff197253ada42be
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-08 13:07:37 +0530

    promise concept tut done

---
+++ CWH-Concepts/js/tut38.js
@@ -0,0 +1,4 @@
+// You have pretend to use a word api which will contain an object and you hve to print definition from all the results of that word api.
+// YOu have to take input from an input tag.
+// You have to print it in the dom
+// If you are using bootstrap then its a plus
