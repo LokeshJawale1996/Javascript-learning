1121fa15ee75075cb8e585a8a5079860b8ffdd43
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-08 21:07:24 +0530

    error handling session done

---
+++ CWH-Concepts/js/tut44.js
@@ -0,0 +1,33 @@
+console.log("This is tutorial 44");
+
+// Pretend this is coming from a server as response
+let a = "Harry bhai";
+a = undefined;
+if (a != undefined) {
+  throw new Error("This is not undefined");
+} else {
+  console.log("This is undefined");
+}
+
+// try {
+//   null.console;
+//   console.log("We are inside try block");
+
+//   functionHarry();
+// } catch (error) {
+//   console.log(error);
+//   console.log("Are you okay?");
+//   console.log(error.name);
+//   console.log(error.message);
+// } finally {
+//   console.log("Finally we will run this");
+// }
+
+try {
+  console.log("This statement works");
+  throw new Error("This statement throws an error");
+} catch (error) {
+  console.log("Error has been handled");
+} finally {
+  console.log("Everything has been handled");
+}
