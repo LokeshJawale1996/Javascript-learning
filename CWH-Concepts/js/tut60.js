7ad35daa385e775fbb9867f5ddda6a5112d7d291
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-13 22:02:48 +0530

    array destructing

---
+++ CWH-Concepts/js/tut60.js
@@ -0,0 +1,34 @@
+console.log("This is tutorial 60");
+//Destructing in JavaScript
+
+// let a, b;
+// [a, b] = [34, 564];
+// console.log(a, b);
+[a, b, c, ...d] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+//Array Destructuring
+({ a, b, c, ...d } = { a: 34, b: 345, c: 67, d: 45, e: 34 });
+console.log(a, b, c, d);
+
+const fruits = ["Apple", "Bananas", "Mangos"];
+[a, b, c] = fruits;
+console.log(a, b, c);
+
+///Object Destructing
+laptop = {
+  model: "Hp pavillion",
+  age: "23 days",
+  gender: "male",
+  net: 1233,
+  start: function () {
+    console.log("started");
+  },
+};
+
+const { model, age, gender, net, start } = laptop;
+console.log(model, age, gender, net, start);
+start();
