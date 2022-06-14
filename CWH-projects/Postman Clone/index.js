82da19036b1e699470ced56e6ddf16cf34b96b50
Lokesh Jawale <105481574+LokeshJawale1996@users.noreply.github.com>
2022-06-14 22:55:45 +0530

    Add files via upload
    postman clone in process'

--- CWH-projects/Postman Clone/index.js
+++ CWH-projects/Postman Clone/index.js
@@ -1,1 +1,66 @@
 console.log("This is my Postman clone");
+
+//Utility Function
+//1.utility function to get element from string
+function getElementFromString(string) {
+  let div = document.createElement("div");
+  div.innerHTML = string;
+  return div.firstElementChild;
+}
+
+//Initialize number of parameters
+let addedParamsCount = 0;
+
+//Hide the parameters box initially
+let parametersBox = document.getElementById("parametersBox");
+parametersBox.style.display = "none";
+
+//If the user clicks on params box,hide the json box
+let paramsRadio = document.getElementById("paramsRadio");
+paramsRadio.addEventListener("click", () => {
+  document.getElementById("requestJsonBox").style.display = "none";
+  document.getElementById("parametersBox").style.display = "block";
+});
+
+//If the user clicks on json box,hide the params box
+let jsonRadio = document.getElementById("jsonRadio");
+jsonRadio.addEventListener("click", () => {
+  document.getElementById("parametersBox").style.display = "none";
+  document.getElementById("requestJsonBox").style.display = "block";
+});
+
+//If the user clicks on + button, add more parameters
+
+let addParam = document.getElementById("addParam");
+addParam.addEventListener("click", () => {
+  let params = document.getElementById("params");
+  let string = `
+   <div id="parametersBox">
+        <div class="form-row">
+          <label for="url" class="col-sm-2 col-form-label">Parameter 
+          ${addedParamsCount + 2}</label>
+          <div class="col-md-4">
+            <input
+              type="email"
+              class="form-control"
+              id="parameterKey${addedParamsCount + 2}"
+              placeholder="Enter Parameter ${addedParamsCount + 2} Key"
+            />
+          </div>
+          <div class="col-md-4">
+            <input
+              type="password"
+              class="form-control"
+              id="parameterValue${addedParamsCount + 2}"
+              placeholder="Enter parameter ${addedParamsCount + 2} Value"
+            />
+          </div>
+          <button id="addParam" class="btn btn-primary">-</button>
+        </div>
+      </div>`;
+  //Convert the element string to DOM node
+  let paramElement = getElementFromString(string);
+  params.appendChild(paramElement);
+  // console.log(paramsElement);
+  addedParamsCount++;
+});
